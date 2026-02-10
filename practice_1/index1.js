8d8534572e7044cc62eb53a5d2660b24009a5d86
Deepak-pd21 <dpddeepak501@gmail.com>
2026-02-10 16:19:33 +0530

    practice

--- practice_1/index1.js
+++ practice_1/index1.js
@@ -2,9 +2,41 @@
 //  1.declaration    let x;
 //  2.assignment  x= 100;
 
- let age = 25;
- let price = 12.99;
- let gpa = 2.1;
+/* VARIABLE TYPES*/
 
- console.log(gpa);
+// NUMBERS (VARIABLE TYPE)
+
+//  let age = 22;
+//  let price = 12.99;
+//  let gpa = 2.1;
+//  console.log("gpa");
+//  console.log(`You are ${age} years old ?`);
+//  console.log(`does it cost $${price} for real!!!`);
+//  console.log(`i just got my gpa checked its ${gpa}`);
+//  console.log(typeof gpa , typeof price);
  
+//STRING (VARIABLE TYPE)
+
+// let firstName = "Jango";
+// let Favsports = "Football";
+// console.log(firstName);
+// console.log(typeof firstName);
+// console.log(`Youre name is ${firstName}?`);
+// console.log(`So you do like ${Favsports}`);
+// console.log(`${firstName} always like ${Favsports} i know that.`);
+
+// BOOLEANS (VARIABLE TYPE)
+// let online = false;
+// let sold = true;
+// console.log(`jango is now online: ${online}`);
+// console.log(`The new car has already been sold out: ${sold}`);
+
+let fullName = "Jango";
+let age = 25;
+let student = false;
+
+document.getElementById("p1").textContent = `your name is ${fullName} right?`;
+document.getElementById("p2").textContent = `its given your age is ${age}`;
+document.getElementById("p3").textContent = `are you a student now: ${student}`;
+
+
