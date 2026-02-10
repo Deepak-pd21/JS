a9e0f8ef5e1caaf4372ed41fb462599b29b270e0
Deepak-pd21 <dpddeepak501@gmail.com>
2026-02-10 12:31:18 +0530

    practice

---
+++ index.js
@@ -0,0 +1,9 @@
+    console.log(`Hello`);
+    console.log(`hiiiii`);
+
+    window.alert(`no tresspassing allowed`);
+    window.alert(`go back!!!!!`);
+
+
+    document.getElementById("myh").textContent = 'Hello';
+    document.getElementById("myp").textContent = `Fahhhhh`;
