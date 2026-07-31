c8f22df8e476169bfe00938cfc5b0a9a6c181d3e
Deepak-pd21 <dpddeepak501@gmail.com>
2026-07-31 10:34:24 +0530

    practice

--- practice_1/index1.js
+++ practice_1/index1.js
@@ -1,2 +1,7 @@
 // type conversion = change in datatype of a value to another
 //                   (strings,numbers,Booleans)
+let age = window.prompt("How old are you?");
+
+age+=1;
+
+console.log(age, typeof age);
